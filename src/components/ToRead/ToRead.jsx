f5ab53892ad4cbb734b925c54d5c95d78b868d24
Dana Musilova <dana.musilova@email.cz>
2021-06-06 09:58:27 +0200

    knihy k přečtení - základní úprava záznamů

--- src/components/ToRead/ToRead.jsx
+++ src/components/ToRead/ToRead.jsx
@@ -21,35 +21,41 @@ const ToRead = () => {
   return (
     <ul>
       {toRead.map((book) => (
-        <li key={book.id}>
-          {book.toRead}
-
-          <button
-            id={book.id}
-            className={book.checked ? 'toread--checked' : 'toread'}
-            onClick={() => {
-              db.collection('ToRead')
-                .doc(book.id)
-                .update({ checked: !book.checked });
-            }}
-          >
-            Splněno
-          </button>
-
-          <button
-            className="delete"
-            onClick={() => {
-              const confirmation = confirm('Opravdu chceš záznam smazat?');
-              {
-                confirmation
-                  ? db.collection('ToRead').doc(book.id).delete()
-                  : null;
+        <div className="toread__box" key={book.id}>
+          <div className="toread__box1">
+            <button
+              id={book.id}
+              className={
+                book.checked
+                  ? 'toread__check toread__check--checked'
+                  : 'toread__check'
               }
-            }}
-          >
-            Smazat
-          </button>
-        </li>
+              onClick={() => {
+                db.collection('ToRead')
+                  .doc(book.id)
+                  .update({ checked: !book.checked });
+              }}
+            >
+              Splněno
+            </button>
+            <li key={book.id}>{book.toRead} </li>
+          </div>
+          <div className="toread__box2">
+            <button
+              className="delete"
+              onClick={() => {
+                const confirmation = confirm('Opravdu chceš záznam smazat?');
+                {
+                  confirmation
+                    ? db.collection('ToRead').doc(book.id).delete()
+                    : null;
+                }
+              }}
+            >
+              Smazat
+            </button>
+          </div>
+        </div>
       ))}
     </ul>
   );
