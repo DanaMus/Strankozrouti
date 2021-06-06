9f430810e2acb30b2160c023989fedd3e1f92287
tuolomee <kejmarova@gmail.com>
2021-06-06 18:27:30 +0200

    odstranění patičky u konkrétní přečtené knihy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect }from 'react';
+import { useLocation } from 'react-router-dom';
 import './style.css';
 
 export const Footer = () => {
 
+  const location = useLocation();
+  useEffect(() => {
+  console.log(location.pathname);
+  },[location])
 
-  return (
+  const regex = /prectene\/(.*)/g;
+if (!location.pathname.match(regex)) { return (
     
   <footer>
     <p>Tento projekt byl vytvořen v rámci Digitální akademie</p>
     <img className='czechitas_logo' src='/assets/czechitas.png' alt='czechitaslogo'></img>
   </footer>
 
-  )
+  )} else {
+    return <></>
+  }
 }
