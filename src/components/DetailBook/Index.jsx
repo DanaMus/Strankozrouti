f6dfd83d964809202b7c53ce8427e40c7d253d82
Dana Musilova <dana.musilova@email.cz>
2021-06-06 14:53:54 +0200

    detail knihy - stylování mobilní verze

--- src/components/DetailBook/Index.jsx
+++ src/components/DetailBook/Index.jsx
@@ -19,22 +19,61 @@ export const DetailBook = () => {
   return (
     <div className="book__container">
       {/* <h3>ID knížky: {id}</h3> */}
-      <h3>Autor: {bookDetail.authorName}</h3>
-      <h3>Název: {bookDetail.bookName}</h3>
-      <p>Date: {bookDetail.date}</p>
-      <p>Počet stránek: {bookDetail.pages}</p>
-      {bookDetail.rating ? <p>Hodnocení: {bookDetail.rating}</p> : null}
-      {bookDetail.genre ? <p>Citát, myšlenka: {bookDetail.genre}</p> : null}
-      {bookDetail.characters ? (
-        <p>Hlavní postavy: {bookDetail.characters}</p>
+      <div className="singledetail__container">
+        <h2 className="singledetail__heading--2">{bookDetail.authorName}</h2>
+      </div>
+      <div className="singledetail__container">
+        <h1 className="singledetail__heading--1">{bookDetail.bookName}</h1>
+      </div>
+      <div className="singledetail__container">
+        <div className="singledetail__name">Datum přečtení: </div>
+        <div className="singledetail__item">{bookDetail.date}</div>
+      </div>
+      <div className="singledetail__container">
+        <div className="singledetail__name">Počet stránek: </div>
+        <div className="singledetail__item">{bookDetail.pages}</div>
+      </div>
+      {bookDetail.rating ? (
+        <div className="singledetail__container">
+          <div className="singledetail__name">Hodnocení: </div>
+          <div className="singledetail__item">{bookDetail.rating}</div>
+        </div>
       ) : null}
-      {bookDetail.content ? <p>Obsah: {bookDetail.content}</p> : null}
-      {bookDetail.motto ? <p>Citát, myšlenka: {bookDetail.motto}</p> : null}
+      {bookDetail.genre ? (
+        <div className="singledetail__container">
+          <div className="singledetail__name">Žánr: </div>
+          <div className="singledetail__item">{bookDetail.genre}</div>
+        </div>
+      ) : null}
+      {bookDetail.characters ? (
+        <div className="singledetail__container">
+          <div className="singledetail__name">Hlavní postavy: </div>
+          <div className="singledetail__item">{bookDetail.characters}</div>
+        </div>
+      ) : null}
+      {bookDetail.content ? (
+        <div className="singledetail__container">
+          <div className="singledetail__name">Obsah: </div>
+          <div className="singledetail__item">{bookDetail.content}</div>
+        </div>
+      ) : null}
+      {bookDetail.motto ? (
+        <div className="singledetail__container">
+          <div className="singledetail__name">Citát, zajímavá myšlenka: </div>
+          <div className="singledetail__item">{bookDetail.motto}</div>
+        </div>
+      ) : null}
       {bookDetail.recommendation ? (
-        <p>Doporučení kamarádovi: {bookDetail.recommendation}</p>
+        <div className="singledetail__container">
+          <div className="singledetail__name">Doporučení kamarádovi: </div>
+          <div className="singledetail__item">{bookDetail.recommendation}</div>
+        </div>
       ) : null}
       {bookDetail.optional ? (
-        <p>Ostatní poznámky: {bookDetail.optional}</p>
+        <div className="singledetail__container">
+          <div className="singledetail__name">Další poznámky: </div>
+          <div className="singledetail__item">{bookDetail.optional}</div>
+        </div>
       ) : null}
       <button onClick={window.print}>Vytisknout</button>
       {/* <h2>{film.jmeno}</h2>
