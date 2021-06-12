4a98c9ed25f25fd535753fc329d4b8a9622469d9
tuolomee <kejmarova@gmail.com>
2021-06-12 10:12:51 +0200

    uprava BEM Homepage, InfoIcon

--- src/components/InfoIcon/InfoIcon.jsx
+++ src/components/InfoIcon/InfoIcon.jsx
@@ -6,35 +6,35 @@ export const InfoIcon = () => {
 
   return (
     
-    <div className="infoicon_container">
+    <div className="infoicon__container">
       
       <div className="infoicon">
-    <div className="infoicon--image">
-    <img className="infoicon--image_monster"src='assets/jednoduse.svg'></img>
+    <div className="infoicon__image">
+    <img className="infoicon__image--monster"src='assets/jednoduse.svg'></img>
    </div>
-      <div className="infoicon--description">
+      <div className="infoicon__description">
       <h3>Jednoduše</h3>
-      <p>Nevíš co zapsat do čtenářského deníčku? Neznáš jeho strukturu? Náš formulář tě navede. Vyplň přednastavená políčka a pokud potřebuješ, přidej si vlastní.</p>
+      <p>Nevíš co zapsat do čtenářského deníčku? Neznáš jeho strukturu? Náš formulář tě navede. Vyplň přednastavená políčka a pokud potřebuješ, můžeš použít políčko "ostatní".</p>
       </div>
       </div>
 
       <div className="infoicon">
-    <div className="infoicon--image">
-    <img className="infoicon--image_monster"src='assets/prehledne.svg'></img>
+    <div className="infoicon__image">
+    <img className="infoicon__image--monster"src='assets/prehledne.svg'></img>
    </div>
-      <div className="infoicon--description">
+      <div className="infoicon__description">
       <h3>Přehledně</h3>
-      <p>Všechny zapsané knížky najdeš přehledně pod záložkou přečtené knihy. Odtud si můžeš svůj záznam vytisknout nebo uložit.</p>
+      <p>Všechny zapsané knížky najdeš přehledně pod záložkou "přečtené". Odtud si můžeš svůj záznam vytisknout nebo uložit.</p>
       </div>
       </div>
 
       <div className="infoicon">
-    <div className="infoicon--image">
-    <img className="infoicon--image_monster"src='assets/zabavne.svg'></img>
+    <div className="infoicon__image">
+    <img className="infoicon__image--monster"src='assets/zabavne.svg'></img>
    </div>
-      <div className="infoicon--description">
+      <div className="infoicon__description">
       <h3>Zábavně</h3>
-      <p>Každou přečtenou knížkou nakrmíš stránkožrouta. On ti za to rád poví kolik stránek už máš přečtených a připomene ti co číst dál.</p>
+      <p>Každou přečtenou knížkou nakrmíš Stránkožrouta. On ti za to rád poví, kolik stránek a knížek už máš přečtených.</p>
       </div>
       </div>
       </div>
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -8,9 +8,9 @@ import './style.css';
 export const Homepage = () => {
   return (
     <>
-      <div className="homepage_container">
-        <div className="hero_container">
-          <div className="hero--description">
+      <div className="homepage__container">
+        <div className="hero__container">
+          <div className="hero__description">
             <h1>Čtenářský deník</h1>
             <h1>jednoduše a zábavně!</h1>
             <Link className="hero__link" to="/detailni">
@@ -24,9 +24,9 @@ export const Homepage = () => {
               </Button>
             </Link>
           </div>
-          <div className="hero--image">
+          <div className="hero__image">
             <img
-              className="hero--monster"
+              className="hero__monster"
               src="assets/monsterlibraryleft.svg"
             ></img>
           </div>
