c717276d2f642046696d9254ae69816326ac66b8
Dana Musilova <dana.musilova@email.cz>
2021-06-12 12:28:06 +0200

    přečtené - vycentrování

--- src/pages/BookListPage/index.jsx
+++ src/pages/BookListPage/index.jsx
@@ -5,13 +5,16 @@ import './style.css';
 const BookListPage = () => {
   return (
     <>
-    <div className="booklist__wrap">
-    <div className="booklist__description">
-    <p>Tady najdeš přehled tvých přečtených knížek. Po rozkliknutí se ti zobrazí detail. Záznam si také můžeš vytisknout a odevzdat ve škole.</p>
-    <img src="/assets/violet.svg"></img>
-    
-      <BookList />
-      </div>
+      <div className="booklist__wrap">
+        <div className="booklist__description">
+          <p>
+            Tady najdeš přehled tvých přečtených knížek. Po rozkliknutí se ti
+            zobrazí detail. Záznam si také můžeš vytisknout a odevzdat ve škole.
+          </p>
+          <img className="booklist_image" src="/assets/violet.svg"></img>
+
+          <BookList />
+        </div>
       </div>
     </>
   );
